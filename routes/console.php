e62adce16e5751f8b977d0f41efdcde9daff664e
Egwu15 <egwutedd@gmail.com>
2025-09-20 10:09:25 +0100

    added queue process jobs

--- routes/console.php
+++ routes/console.php
@@ -4,12 +4,18 @@ use App\Jobs\SyncVoidedSubscriptions;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Log;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::job(new SyncVoidedSubscriptions())->dailyAt('3:00');
+Schedule::command('queue:work --stop-when-empty')->everyMinute();
+
+Schedule::call(function () {
+    Log::info("Test schedule is running at " . now());
+})->everyMinute();
+
 Schedule::job(new SyncVoidedSubscriptions())
     ->timezone('africa/bangui')
-    ->everyMinute();
+    ->dailyAt('3:00');
